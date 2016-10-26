4126d407fdc7ae78ba93c470fc44b9e37d5f7b82
Lucas Sampaio Dias <Lucas Sampaio Dias>
2016-10-26 12:21:13 -0200

    Variaveis finais de máscara adicionadas

--- src/main/java/com/github/kyriosdata/exemplo/Cpf.java
+++ src/main/java/com/github/kyriosdata/exemplo/Cpf.java
@@ -18,6 +18,50 @@ public final class Cpf {
      * Número de dígitos do CPF.
      */
     private static final int DIGITOS_CPF = 11;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_A = 0;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_B = 1;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_C = 2;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_D = 3;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_E = 4;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_F = 5;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_G = 6;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_H = 7;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_I = 8;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_J = 9;
+    /**
+     * Código usado referente a máscara do CPF (abc.def.ghi-jk).
+     */
+    private static final int COD_K = 10;
 
     /**
      * Método 1 de verificação da validade de um cpf.
@@ -27,13 +71,18 @@ public final class Cpf {
      */
     public static boolean cpf(final int[] d) {
         if (verificaDigitos(d, DIGITOS_CPF)) {
-            int sj = d[0] + (2 * d[1]) + (3 * d[2]) + (4 * d[3]) + (5 * d[4])
-                    + (6 * d[5]) + (7 * d[6]) + (8 * d[7]) + (9 * d[8]);
-            int sk = d[1] + (2 * d[2]) + (3 * d[3]) + (4 * d[4]) + (5 * d[5])
-                    + (6 * d[6]) + (7 * d[7]) + (8 * d[8]) + (9 * d[9]);
-            int j = mod(mod(sj, 11), 10);
-            int k = mod(mod(sk, 11), 10);
-            return j == d[9] && k == d[10];
+            int sj = d[COD_A] + ((COD_B + 1) * d[COD_B])
+                    + ((COD_C + 1) * d[COD_C]) + ((COD_D + 1) * d[COD_D])
+                    + ((COD_E + 1) * d[COD_E]) + ((COD_F + 1) * d[COD_F])
+                    + ((COD_G + 1) * d[COD_G]) + ((COD_H + 1) * d[COD_H])
+                    + ((COD_I + 1) * d[COD_I]);
+            int sk = d[COD_B] + (COD_C * d[COD_C]) + (COD_D * d[COD_D])
+                    + (COD_E * d[COD_E]) + (COD_F * d[COD_F])
+                    + (COD_G * d[COD_G]) + (COD_H * d[COD_H])
+                    + (COD_I * d[COD_I]) + (COD_J * d[COD_J]);
+            int j = mod(mod(sj, DIGITOS_CPF), COD_K);
+            int k = mod(mod(sk, DIGITOS_CPF), COD_K);
+            return j == d[COD_J] && k == d[COD_K];
         }
         return false;
     }
@@ -46,19 +95,19 @@ public final class Cpf {
      */
     public static boolean cpf2(final int[] d) {
         if (verificaDigitos(d, DIGITOS_CPF)) {
-            int c = 7;
-            int p = d[8];
-            int s = d[8];
+            int c = COD_H;
+            int p = d[COD_I];
+            int s = d[COD_I];
 
             while (0 <= c) {
                 p += d[c];
                 s += p;
                 c--;
             }
-            int j = mod(mod(s, 11), 10);
-            int k = mod(mod(s - p + (9 * j), 11), 10);
+            int j = mod(mod(s, DIGITOS_CPF), COD_K);
+            int k = mod(mod(s - p + (COD_J * j), DIGITOS_CPF), COD_K);
 
-            return j == d[9] && k == d[10];
+            return j == d[COD_J] && k == d[COD_K];
         }
         return false;
     }
